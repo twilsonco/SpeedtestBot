2441da7c4dc50c3c3f6cb60dec1cac411ac6916b
Tim Wilson <twilsonco@gmail.com>
2022-03-07 10:55:56 -0700

    Switch to using json output from speedtest
    Should render embed correctly every time now.

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 """
-Copyright © twilsonco 2020
+Copyright © twilsonco 2022
 Description: This is a discord bot to run an internet speedtest on your server
 
-Version: 1.2
+Version: 1.3
 """
 
 import discord
@@ -26,7 +26,8 @@ OWNERS = [OWNER_USER_IDS]
 BLACKLIST = [USER_IDS]
 WHITELIST = [USER_IDS]
 CHANNEL_IDS=[CHANNEL_IDS]
-SPEEDTEST_PATH='/usr/local/bin/speedtest'
+SPEEDTEST_TEST=['/usr/local/bin/speedtest','-f','json']
+SPEEDTEST_VERSION=['/usr/local/bin/speedtest','--version']
 
 
 client = Bot(command_prefix=BOT_PREFIX)
@@ -45,7 +46,7 @@ async def on_ready():
 	print('-------------------')
 
 
-def humanbytes(B):
+def humanbytes(B, time_string=''):
 	'Return the given bytes as a human friendly KB, MB, GB, or TB string'
 	B = float(B)
 	KB = float(1024)
@@ -54,27 +55,90 @@ def humanbytes(B):
 	TB = float(KB ** 4) # 1,099,511,627,776
 
 	if B < KB:
-		return '{0} {1}'.format(B,'B')
+		return '{0} B'.format(B) + time_string
 	elif KB <= B < MB:
-		return '{0:.2f} kB'.format(B/KB)
+		return '{0:.2f} kB'.format(B/KB) + time_string
 	elif MB <= B < GB:
-		return '{0:.2f} MB'.format(B/MB)
+		return '{0:.2f} MB'.format(B/MB) + time_string
 	elif GB <= B < TB:
-		return '{0:.2f} GB'.format(B/GB)
+		return '{0:.2f} GB'.format(B/GB) + time_string
 	elif TB <= B:
-		return '{0:.2f} TB'.format(B/TB)
-	  
-def tobytes(B):
-	'Return the number of bytes given by a string (a float followed by a space and the unit of prefix-bytes eg. "21.34 GB")'
-	numstr = B.lower().split(' ')
-	KB = (('kilo','kb','kb/s'),float(1024))
-	MB = (('mega','mb','mb/s'),float(KB[1] ** 2)) # 1,048,576
-	GB = (('giga','gb','gb/s'),float(KB[1] ** 3)) # 1,073,741,824
-	TB = (('tera','tb','tb/s'),float(KB[1] ** 4)) # 1,099,511,627,776
+		return '{0:.2f} TB'.format(B/TB) + time_string
+
+""" example speedtest result in json
+		{
+		    "type": "result",
+		    "timestamp": "2022-03-07T17:15:19Z",
+		    "ping": {
+		        "jitter": 0.81399999999999995,
+		        "latency": 2.3500000000000001
+		    },
+		    "download": {
+		        "bandwidth": 116707935,
+		        "bytes": 676104480,
+		        "elapsed": 5807
+		    },
+		    "upload": {
+		        "bandwidth": 113990420,
+		        "bytes": 1226035930,
+		        "elapsed": 12005
+		    },
+		    "packetLoss": 0,
+		    "isp": "CenturyLink",
+		    "interface": {
+		        "internalIp": "10.0.1.70",
+		        "name": "en0",
+		        "macAddr": "A8:20:66:45:B9:D8",
+		        "isVpn": false,
+		        "externalIp": "97.118.183.106"
+		    },
+		    "server": {
+		        "id": 8862,
+		        "name": "CenturyLink",
+		        "location": "Denver, CO",
+		        "country": "United States",
+		        "host": "denver.speedtest.centurylink.net",
+		        "port": 8080,
+		        "ip": "205.171.253.6"
+		    },
+		    "result": {
+		        "id": "bdb855ba-9533-4f60-b561-a6573f0e2831",
+		        "url": "https://www.speedtest.net/result/c/bdb855ba-9533-4f60-b561-a6573f0e2831"
+		    }
+		}
+"""
+def st_to_embed(st,ts):
+	embed=discord.Embed(title="Speedtest results (view online)", url=st["result"]["url"], color=0x53d6fd)
 	
-	for prefix in (KB,MB,GB,TB):
-		if numstr[1] in prefix[0]:
-			return float(float(numstr[0]) * prefix[1])
+	embed.set_thumbnail(url="https://www.userlogos.org/files/logos/8596_famecky/st.png")
+	
+	embed.add_field(name="⬇️ %s" % (humanbytes(st["download"]["bandwidth"],'/s')), value="⬆️ %s" % (humanbytes(st["upload"]["bandwidth"],'/s')), inline=True)
+	
+	embed.add_field(name="⚡️ %.2f ms Ping" % (st["ping"]["latency"]), value="〽️ %.2f ms Jitter" % (st["ping"]["jitter"]), inline=True)
+	
+	embed.set_footer(text="packet loss: %.2f%%\ndata used: %s down / %s up / %s total\nserver: %s\nISP: %s" % (st["packetLoss"], 
+	humanbytes(st["download"]["bytes"]),
+	humanbytes(st["upload"]["bytes"]),
+	humanbytes(st["download"]["bytes"]+st["upload"]["bytes"]),
+	f'{st["server"]["name"]} — {st["server"]["location"]}, {st["server"]["country"]} (id = {st["server"]["id"]})', 
+	st["isp"]))
+	
+	embed.timestamp = ts
+	return embed
+
+@client.command(name='last', pass_context=True)
+async def lasttest(context):
+	if len(CHANNEL_IDS) > 0 and context.message.channel.id not in CHANNEL_IDS:
+		await context.message.channel.send("I don't respond to commands in this channel...")
+		return False
+	else:
+		await context.message.channel.send('Showing results of last speedtest')
+		st = LASTSPEEDTEST
+		ts = LASTSPEEDTESTTIMESTAMP
+		if st is not None and ts is not None:
+			await context.message.channel.send(embed=st_to_embed(st,ts))
+		else:
+			await context.message.channel.send("No previous speedtest results to show...")
 
 @client.command(name='test', pass_context=True)
 async def test(context, *, content = ""):
@@ -86,42 +150,18 @@ async def test(context, *, content = ""):
 		await context.message.channel.send("I don't respond to commands in this channel...")
 		return False
 	elif context.message.author.id in BLACKLIST or (len(WHITELIST) > 0 and context.message.author.id not in WHITELIST) and LASTSPEEDTEST is not None:
-		await context.message.channel.send('Showing results of last speedtest')
-		st = LASTSPEEDTEST
-		ts = LASTSPEEDTESTTIMESTAMP
+		await lasttest(context)
 	else:
 		if content == "":
 			await context.message.channel.send('Running speedtest on server (not on your local machine). Please wait...')
-			st = subprocess.check_output([SPEEDTEST_PATH]).decode()
-			ts = datetime.datetime.now(tz=pytz.timezone('America/Denver'))
-			LASTSPEEDTEST = st
-			LASTSPEEDTESTTIMESTAMP = ts
-			stinfo = {i[0]:re.search(i[1],st) for i in [
-				['server','Server:\s*(.*)\n'],
-				['isp','ISP:\s*(.*)\n'],
-				['lat','Latency:\s*([0-9\.]+)\s*(.*?)\s*\(([0-9\.]+)\s*([a-z]+)\s*?.*?\)'],
-				['down','Download:\s*([0-9\.]+)\s*(.*?)\s*\(.*?:\s*?([0-9\.]+ .*?)\s*?\)'],
-				['up','Upload:\s*([0-9\.]+)\s*(.*?)\s*\(.*?:\s*?([0-9\.]+.*?)\s*?\)'],
-				['pack','Packet Loss:\s*(.+)\n'],
-				['url','Result URL:\s*(.*)']
-			]}
-			if None not in stinfo.values():
-				embed=discord.Embed(title="Speedtest results (view online)", url=stinfo['url'].group(1), color=0x53d6fd)
-				embed.set_thumbnail(url="https://www.userlogos.org/files/logos/8596_famecky/st.png")
-				embed.add_field(name="⬇️ %s %s" % (stinfo['down'].group(1),stinfo['down'].group(2)), value="⬆️ %s %s" % (stinfo['up'].group(1),stinfo['up'].group(2)), inline=True)
-				embed.add_field(name="⚡️ %s %s Ping" % (stinfo['lat'].group(1),stinfo['lat'].group(2)), value="〽️ %s %s Jitter" % (stinfo['lat'].group(3),stinfo['lat'].group(4)), inline=True)
-				embed.set_footer(text="packet loss: %s\ndata used: %s down / %s up / %s total\nserver: %s\nISP: %s" % (stinfo['pack'].group(1), 
-				stinfo['down'].group(3),
-				stinfo['up'].group(3),
-				humanbytes(tobytes(stinfo['down'].group(3))+tobytes(stinfo['up'].group(3))),
-				stinfo['server'].group(1), 
-				stinfo['isp'].group(1)))
-				embed.timestamp = ts
-				await context.message.channel.send(embed=embed)
-			else:
-				await context.message.channel.send(st)
+			async with context.channel.typing():
+				st = json.loads(subprocess.check_output(SPEEDTEST_TEST).decode())
+				ts = datetime.datetime.now(tz=pytz.timezone('America/Denver'))
+				LASTSPEEDTEST = st
+				LASTSPEEDTESTTIMESTAMP = ts
+				await context.message.channel.send(embed=st_to_embed(st,ts))
 		elif content == "info":
-			testResult = subprocess.check_output([SPEEDTEST_PATH,'--version'])
+			testResult = subprocess.check_output(SPEEDTEST_VERSION)
 			for l in testResult.decode().split('\n'):
 				if len(l) > 0:
 					await context.message.channel.send(l)
